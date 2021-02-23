e5849ba0c789577bd29b3eb6024d3225e0418c81
haowr <shinedesign@protonmail.com>
2021-02-23 03:19:31 -0800

    INITIAL-COMMIT

---
+++ truffle-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1", //localhost
+      port: 7545, //Gnosche's Port
+      network_id: "*" // Match any network id
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
+}
